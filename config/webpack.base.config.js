2550a49f8c677e4beed92d4c74f373f3a27439ae
liuhw <liuhuansir@126.com>
2019-08-02 09:41:01 +0800

    file-loader替换成url-loader

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -22,15 +22,15 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg|svg|gif)$/,
+        test: /\.(png|jpe?g|svg|gif)$/,
         use: [
-          'file-loader'
+          'url-loader'
         ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader'
+          'url-loader'
         ]
       }
     ]
